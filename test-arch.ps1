80ca9b754925727b66766e29b3c8f55353b4d317
mamasud91 <mamasud91@gmail.com>
2018-02-26 21:08:43 -0600

    Add files via upload

---
+++ test-arch.ps1
@@ -0,0 +1,9 @@
+﻿$PrjToArchive = 'XXXXXXXXX'
+$Path = 'c:\TEMP\Export\PSTraining'
+$pwDocs = Get-PWDocumentsBySearch -FolderPath $ProjectToArchive -GetAttributes
+$pwDocs.Count
+$Export = @{
+    OutputFolder = $Path;
+    ProjectWiseFolder = $PrjToArchive;
+    OutputFileName = "$PrjToArchive.sqlite";
+}
